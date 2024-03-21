9f1c39ebcc4977deedfdb5e8feb4326c0557729b
manascb1344 <zephop76593@gmail.com>
2024-03-22 00:48:49 +0530

    Dash basic

--- frontend/src/Dash.jsx
+++ frontend/src/Dash.jsx
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Dash = () => {
-  return (
-    <div className="flex justify-between w-full">
-      <div className="h-[91vh] rounded-lg bg-gray-300 m-2 flex-1"></div>
-      <div className="h-[91vh] rounded-lg bg-gray-300 m-2 flex-1"></div>
-      <div className="h-[91vh] rounded-lg bg-gray-300 m-2 flex-1"></div>
-    </div>
-  );
+	const totalTransactions = 1000;
+	const suspiciousTransactions = 50;
+	const totalBitcoinInvolved = 250;
+	const suspiciousPercentage =
+		(suspiciousTransactions / totalTransactions) * 100;
+
+	const [animationClass, setAnimationClass] = useState("");
+
+	useEffect(() => {
+		// Set animation class with delay for each div
+		setTimeout(() => setAnimationClass("fadeIn"), 500);
+		setTimeout(() => setAnimationClass("fadeIn"), 1000);
+		setTimeout(() => setAnimationClass("fadeIn"), 1500);
+		setTimeout(() => setAnimationClass("fadeIn"), 2000);
+	}, []);
+
+	return (
+		<div className="flex  w-full">
+			<div className={`flex-1 m-2 animated ${animationClass}`}>
+				<div className="rounded-lg bg-gray-300 p-4">
+					<h2>
+						Total number of transactions analyzed:{" "}
+						{totalTransactions}
+					</h2>
+				</div>
+			</div>
+			<div className={`flex-1 m-2 animated ${animationClass}`}>
+				<div className="rounded-lg bg-gray-300 p-4">
+					<h2>
+						Total number of suspicious transactions detected:{" "}
+						{suspiciousTransactions}
+					</h2>
+				</div>
+			</div>
+			<div className={`flex-1 m-2 animated ${animationClass}`}>
+				<div className="rounded-lg bg-gray-300 p-4">
+					<h2>
+						Total amount of Bitcoin involved in suspicious
+						transactions: {totalBitcoinInvolved} BTC
+					</h2>
+				</div>
+			</div>
+			<div className={`flex-1 m-2 animated ${animationClass}`}>
+				<div className="rounded-lg bg-gray-300 p-4">
+					<h2>
+						Percentage of suspicious transactions compared to total
+						transactions: {suspiciousPercentage.toFixed(2)}%
+					</h2>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dash;
