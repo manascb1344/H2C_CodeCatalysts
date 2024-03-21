b0a2caeaaa3dc581ea8198d6a3bbb2718c3843df
SharanRP <z8903830@gmail.com>
2024-03-21 17:58:01 +0530

    Updated file

--- frontend/src/Dash.jsx
+++ frontend/src/Dash.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Dash = () => {
-	return <div>Hi</div>;
+  return (
+    <div className="flex justify-between w-full">
+      <div className="h-[91vh] rounded-lg bg-gray-300 m-2 flex-1"></div>
+      <div className="h-[91vh] rounded-lg bg-gray-300 m-2 flex-1"></div>
+      <div className="h-[91vh] rounded-lg bg-gray-300 m-2 flex-1"></div>
+    </div>
+  );
 };
 
 export default Dash;
